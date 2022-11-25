15b228d2ed4e62b48dd5d9753b6ae24a8eeec298
seasonyuu <seasonyuu@gmail.com>
2022-11-25 18:04:08 +0800

    set chart view as Area
    Signed-off-by: seasonyuu <seasonyuu@gmail.com>

--- packages/web/src/routes/IncrView.vue
+++ packages/web/src/routes/IncrView.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts" setup>
 import { computed, onMounted, ref, watch } from 'vue';
-import { Line } from '@antv/g2plot';
+import { Area } from '@antv/g2plot';
 import type { IStats } from '@cdata/common/types/stats';
 
 import loadDataset from '@/utils/loadDataset';
@@ -18,33 +18,54 @@ function sum(section: Record<string, number>): number {
   return Object.values(section).reduce((n, sum) => n + sum, 0);
 }
 
-const increasement = computed(() => dataset.value.map((data) => ({
-  time: data.time,
-  value: sum(data.data['新增本土确诊病例']) + sum(data.data['新增本土无症状感染者']),
-})));
+const increasement = computed(() =>
+  dataset.value
+    .map((data) => [
+      {
+        type: '新增本土确诊',
+        time: data.time,
+        value: sum(data.data['新增本土确诊病例']),
+      },
+      {
+        type: '新增本土无症状',
+        time: data.time,
+        value: sum(data.data['新增本土无症状感染者']),
+      },
+    ])
+    .flat()
+);
 
 const chart = ref<HTMLElement>();
-let linePlot: Line;
+let area: Area;
 onMounted(() => {
   if (!chart.value) return;
-  linePlot = new Line(chart.value, {
+  area = new Area(chart.value, {
     height: 500,
     data: increasement.value,
     xField: 'time',
     yField: 'value',
-    label: {},
-    point: {},
-    meta: {
-      value: {
-        alias: '新增',
+    seriesField: 'type',
+    tooltip: {
+      customItems: (originalItems) => {
+        let count = 0;
+        originalItems.forEach((item) => (count += Number(item.value)));
+        originalItems.push({
+          name: '总计',
+          value: count,
+          data: {},
+          mappingData: {},
+          color: '',
+          marker: '',
+        });
+        return originalItems;
       },
     },
   });
-  linePlot.render();
+  area.render();
 });
 watch(increasement, () => {
-  linePlot.update({
+  area.update({
     data: increasement.value,
-  })
+  });
 });
 </script>
