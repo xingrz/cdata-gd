3d20ad968dbcecf92ab4093660d664b2156a89c8
XiNGRZ <hi@xingrz.me>
2022-11-26 22:06:55 +0800

    并行化数据集加载

--- packages/web/src/utils/loadDataset.ts
+++ packages/web/src/utils/loadDataset.ts
@@ -1,20 +1,26 @@
 import dayjs from 'dayjs';
 import http from 'ky';
+import { findLast, isUndefined, range } from 'lodash';
 
 export default async function loadDataset<T>(path: string, depth = 60): Promise<T[]> {
-  const dataset: T[] = [];
+  let dataset: T[] = [];
 
-  for (let i = 0; i < depth; i++) {
-    const time = dayjs().subtract(i + 1, 'days').format('YYYY-MM-DD');
-    try {
-      const data = await http.get(`./data/${path}/${time}.json`).json<T>();
-      dataset.unshift(data);
-    } catch (e) {
-      if (dataset.length > 0) {
-        break;
+  for (let i = 0; i < depth; i += 10) {
+    const series = await Promise.all(range(i, Math.min(depth, i + 10)).map(async (i) => {
+      const time = dayjs().subtract(i + 1, 'days').format('YYYY-MM-DD');
+      try {
+        return await http.get(`./data/${path}/${time}.json`).json<T>();
+      } catch (e) {
+        return undefined;
       }
+    }));
+
+    dataset = [...dataset, ...series.filter((t) => !isUndefined(t)) as Awaited<T>[]];
+
+    if (findLast(series, isUndefined) && dataset.length > 0) {
+      break;
     }
   }
 
-  return dataset;
+  return dataset.reverse();
 }
