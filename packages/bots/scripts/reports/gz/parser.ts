742bb1a3197c89b8b7108723e197827110422198
XiNGRZ <hi@xingrz.me>
2022-11-30 12:27:23 +0800

    规避 2022-11-29 报告街道名带空格问题

--- packages/bots/scripts/reports/gz/parser.ts
+++ packages/bots/scripts/reports/gz/parser.ts
@@ -15,7 +15,7 @@ export function parseReport(input: string): IReportData {
   let type: keyof IReportData | undefined;
   for (const line of input.split('\n')) {
     let match: RegExpMatchArray | null;
-    if (match = line.match(/本土确诊病例([\d\-、 ]+).*居住在([^。，]+)/)) {
+    if (match = line.match(/本土确诊病例([\d\-、 ]+).*居住在([^。，\s]+)/)) {
       type = '本土确诊病例';
       const ids = expand(match[1])
       push(data['本土确诊病例'], 'street', match[2], ids);
@@ -34,7 +34,7 @@ export function parseReport(input: string): IReportData {
       const ids = expand(match[1]);
       push(data['本土确诊病例'], 'street', '集中隔离场所', ids);
       push(data['本土确诊病例'], 'source', match[2], ids);
-    } else if (match = line.match(/本土无症状感染者([\d\-、 ]+).*居住在([^。，]+)/)) {
+    } else if (match = line.match(/本土无症状感染者([\d\-、 ]+).*居住在([^。，\s]+)/)) {
       type = '本土无症状感染者';
       const ids = expand(match[1])
       push(data['本土无症状感染者'], 'street', match[2], ids);
